0dff90a82edea12036b876251aa36bad82869a9f
Signe <sign4731@stud.kea.dk>
2021-04-27 10:40:58 +0200

    js start

--- index.js
+++ index.js
@@ -1,72 +1,52 @@
 import "./sass/style.scss";
 
-// ("use strict");
+("use strict");
 
-// function get() {
-//   fetch("https://ezone-eb75.restdb.io/rest/information", {
-//     method: "get",
-//     headers: {
-//       "Content-Type": "application/json; charset=utf-8",
-//       "x-apikey": "60866ebe28bf9b609975a6df",
-//       "cache-control": "no-cache",
-//     },
-//   })
-//     .then((e) => e.json())
-//     .then((data) => data.forEach(showInformation));
-// }
+window.addEventListener("DOMContentLoaded", init);
 
-// function post() {
-//   const data = {
-//     first_name: "Paul",
-//     last_name: "Poulsen",
-//     age: 7,
-//     platform: ["XBOX", "Playstation 4", "Playstation 5", "PC", "MAC"],
-//     type: ["MMORPG", "Sports", "Racing"],
-//     game: ["Tekken", "FIFA", "Super Mario", "Fortnite"],
-//     areas: ["Hand/eye coordination", "Reaction time"],
-//   };
+function init() {
+  console.log("page is loaded");
+  getData();
+}
 
-//   const postData = JSON.stringify(data);
-//   fetch("https://ezone-eb75.restdb.io/rest/information", {
-//     method: "post",
-//     headers: {
-//       "Content-Type": "application/json; charset=utf-8",
-//       "x-apikey": "60866ebe28bf9b609975a6df",
-//       "cache-control": "no-cache",
-//     },
-//     body: postData,
-//   })
-//     .then((res) => res.json())
-//     .then((data) => showInformation(data));
-// }
+function getData() {
+  const platforms = [];
+  const platformEls = document.querySelectorAll("[name=platform]:checked");
+  platformEls.forEach((el) => platforms.push(el.value));
 
-// function deleteGame(id) {
-//   fetch("https://ezone-eb75.restdb.io/rest/information" + id, {
-//     method: "delete",
-//     headers: {
-//       "Content-Type": "application/json; charset=utf-8",
-//       "x-apikey": "60866ebe28bf9b609975a6df",
-//       "cache-control": "no-cache",
-//     },
-//   })
-//     .then((res) => res.json())
-//     .then((data) => console.log(data));
-// }
+  const types = [];
+  const typeEls = document.querySelectorAll("[name=type]:checked");
+  typeEls.forEach((el) => types.push(el.value));
 
-// function showInformation(information) {
-//   console.log(information);
-//   const template = document.querySelector("template").content;
-//   const copy = template.cloneNode(true);
-//   copy.querySelector(".first_name span").textContent = information.first_name;
-//   copy.querySelector(".last_name span").textContent = information.last_name;
-//   copy.querySelector(".age span").textContent = information.age;
-//   copy.querySelector(".platform span").textContent = information.platform;
-//   copy.querySelector(".type span").textContent = information.type;
-//   copy.querySelector(".game span").textContent = information.game;
-//   copy.querySelector(".areas span").textContent = information.areas;
+  const games = [];
+  const gameEls = document.querySelectorAll("[name=game]:checked");
+  gameEls.forEach((el) => games.push(el.value));
 
-//   document.querySelector("main").appendChild(copy);
-// }
-// get();
+  //   document.querySelector("#submit").addEventListener("click", post);
+}
 
-// document.querySelector(".addGame").addEventListener("click", post);
+function post(data) {
+  console.log("Data is posted");
+  //   const data = {
+  //     first_name: "Paul",
+  //     last_name: "Poulsen",
+  //     age: 7,
+  //     platform: ["XBOX", "Playstation 4", "Playstation 5", "PC", "MAC"],
+  //     type: ["MMORPG", "Sports", "Racing"],
+  //     game: ["Tekken", "FIFA", "Super Mario", "Fortnite"],
+  //     areas: ["Hand/eye coordination", "Reaction time"],
+  //   };
+
+  const postData = JSON.stringify(data);
+  fetch("https://ezone-eb75.restdb.io/rest/information", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": "60866ebe28bf9b609975a6df",
+      "cache-control": "no-cache",
+    },
+    body: postData,
+  })
+    .then((res) => res.json())
+    .then((data) => showInformation(data));
+}
